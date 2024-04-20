6247a02159e19f042714a96d83e0189183123fb2
Nabin <nabin.01least@gmail.com>
2024-04-20 14:32:24 +0545

    update

--- resources/views/website/normal.blade.php
+++ resources/views/website/normal.blade.php
@@ -11,26 +11,33 @@
                 <div class="divider"> / </div>
                 <div>{{$slug1->caption ?? $slug1 }} </div>
             </div>
-            <div class="b-title">{{$slug1->caption ?? $slug1 }}</div>
+            <div class="b-title">{{$slug2->caption ?? $slug2 }}</div>
         </div>
     </div>
     <!-- breadcrumb end -->
     <!-- Normal Start -->
     <div class="container-xxl py-6">
-        <div class="container">
-            <div class="n-content" style="background-color: #F3F6F8;">
-                <br>
-                <!-- <div class="title">{{ $normal->caption  }}</div><br> -->
-                <div class="description"> @php echo $normal->short_content; @endphp </div>
-                <br>
-                <img src="{{ $normal->banner_image }}" class="image" style="width: 100%; height: 60%;">
-                <br>
-                <div class="description"> @php echo $normal->long_content; @endphp  </div>
-                 
+    <div class="container">
+        <div class="n-content" style="background-color: #F3F6F8;">
+            @if (!empty($normal->main_attachment))
+            <div class="manager-message">
+                <img src="/uploads/main_attachment/{{$normal->main_attachment}}" alt="" class="manager-image" style="width: 150px; height: 150px;">
             </div>
+            <br>
+            @endif
+            <!-- <div class="title">{{ $normal->caption }}</div><br> -->
+            <div class="description">@php echo $normal->short_content; @endphp</div>
+            <br>
+            <div class="description">@php echo $normal->long_content; @endphp</div>
+            @if (!empty($normal->banner_image))
+            <img src="{{ $normal->banner_image }}" class="image" style="width: 100%; height: 60%;">
+            <br>
+            @endif
         </div>
     </div>
+</div>
+
+
 
     <!-- Normal End -->
-
 @endsection
